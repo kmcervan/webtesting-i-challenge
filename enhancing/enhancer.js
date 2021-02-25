d9b15a9c75723eafe115472a77ce65c01ddfe7bb
karla-cervantes <cervantes.karla14@gmail.com>
2021-02-24 19:37:42 -0600

    repair, success, fail are passing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,28 @@ module.exports = {
 };
 
 function success(item) {
+  if (item.enchancement < 20){
+    item.enchancement += 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if(item.enchancement < 15 ){
+    item.durability -= 5;
+  }else if (item.enchancement >= 15){
+    if (item.enchancement > 16){
+      item.enchancement -= 1;
+    }
+    item.durability -= 10;
+  }
   return { ...item };
 }
 
 function repair(item) {
+  if (item.durability <= 100){
+    item.durability = 100;
+  }
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
 const enhancer = require('./enhancer.js');
 // test away!
+it('check to see if it works', () => {
+    expect(4).toBe(4)
+})
+
+describe('test items', ()=>{
+    let newItem = { name: 'karla', durability: 19, enchancement: 19};
+    
+    it('restores durability back to 100', ()=>{
+
+        const expected = { name: 'karla', durability: 100, enchancement: 19}
+        const actual = enhancer.repair(newItem);
+        expect(expected).toEqual(actual)
+    })
+
+    it('returns modified object', ()=>{
+        const expected = { name: 'karla', durability: 100, enchancement: 20}
+        const actual = enhancer.success(newItem)
+        expect(expected).toEqual(actual)
+    })
+    it('enhancement fails', ()=>{
+        const failItem = {name: 'karla', durability: 29, enchancement: 18}
+        const actual = { name: 'karla', durability: 19, enchancement: 17}
+        expect(enhancer.fail(failItem)).toEqual(actual)
+    })
+})
